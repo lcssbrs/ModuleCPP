840feacccd9c6dcc0f34d2e4880e1d30cb3390be
lcssbrs <lucas.seib@icloud.com>
2024-01-10 20:13:23 +0100

    add in ex00 a comment on how to use main function

--- M_06/ex00/src/main.cpp
+++ M_06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lseiberr <lseiberr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 10:52:00 by lseiberr          #+#    #+#             */
-/*   Updated: 2024/01/10 15:46:44 by lseiberr         ###   ########.fr       */
+/*   Updated: 2024/01/10 20:12:55 by lseiberr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int main (int ac, char **ag)
 		std::cout << "Put an argument\n";
 		return 0;
 	}
+	//remove if instantiabl class
 	if (ac == 2)
 		std::cout << "not instanciable class yet\n";
 	/*ScalarConverter scal;
