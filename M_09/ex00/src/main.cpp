f0ca9a62ad933abf5283483304b25631e8bcf59c
lcssbrs <lucas.seib@icloud.com>
2024-01-16 20:04:22 +0100

    commit main

--- M_09/ex00/src/main.cpp
+++ M_09/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lseiberr <lseiberr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 09:07:00 by lseiberr          #+#    #+#             */
-/*   Updated: 2024/01/16 18:54:25 by lseiberr         ###   ########.fr       */
+/*   Updated: 2024/01/16 20:03:06 by lseiberr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,21 @@ int	main(int ac, char **ag)
 	map_txt = open_data((std::string)ag[1]);
 	map::iterator itr_txt = map_txt.begin();
 	map::iterator itr_data = map_data.begin();
-	while (itr_txt != map_txt.end())
+	while (itr_data != map_data.end())
 	{
-		itr_data = map_data.begin();
-		while (itr_data != map_data.end())
+		itr_txt = map_txt.begin();
+		while (itr_txt != map_txt.end())
 		{
-			if (itr_txt->first.compare(itr_data->first) == 0)
+			if (itr_data->first.compare(itr_txt->first) == 0)
 			{
-				std::cout << stof(itr_txt->second) * stof(itr_data->second) << std::endl;
+				if (itr_data->second.find((std::string)"error") != std::string::npos || itr_txt->second.find((std::string)"error") != std::string::npos)
+					std::cout << "Error\n";
+				else
+					std::cout << stof(itr_txt->second) * stof(itr_data->second) << std::endl;
 			}
-			itr_data++;
+			itr_txt++;
 		}
-		itr_txt++;
+		itr_data++;
 	}
 /*
 	map::iterator itr = map_txt.begin();
